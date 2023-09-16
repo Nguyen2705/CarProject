5c033a80da00a11b851c209e329a87f64b8b7afc
hlnguyenlinh11 <112446306+hlnguyenlinh11@users.noreply.github.com>
2023-09-16 18:39:06 -0500

    Signup

--- CarProject/components/Screens/SignUpScreen.js
+++ CarProject/components/Screens/SignUpScreen.js
@@ -1,87 +1,70 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
-import * as firebase from 'firebase';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDApbcZJQrNmvE8n2h-xFrNNivyuUgqcKc",
-  authDomain: "car-project-b12f9.firebaseapp.com",
-  projectId: "car-project-b12f9",
-  storageBucket: "car-project-b12f9.appspot.com",
-  messagingSenderId: "808665816378",
-  appId: "1:808665816378:web:88edbc64a587909ffe4403",
-  measurementId: "G-P609EPJE9M"
-};
-
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
-
+import { View, Text, TextInput, Button, StyleSheet, KeyboardAvoidingView, TouchableOpacity } from 'react-native';
+import firebase from 'firebase/app';
 
 function SignUpScreen({ navigation }) {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  // const handleSignUp = () => {
-  //   // Implement your user registration logic here, e.g., with Firebase or a backend API.
-  //   // For this example, we'll just log the user's data to the console.
-  //   console.log('Name:', name);
-  //   console.log('Email:', email);
-  //   console.log('Password:', password);
-
-  //   // You can also navigate to another screen after registration.
-  //   // For example, navigate to the Login screen:
-  //   // navigation.navigate('Login');
-  // };
-
-  const handleSignUp = async (e) => {
-    e.preventDefault();
-
-    try {
-        const userCredential = await firebase.auth().createUserWithEmailAndPassword(email, password); 
-
-        const user = userCredential.user; 
-        console.log('User registered: ', user); 
-    } catch ( error ) {
-        cosole.error('Error register: ', error.message); 
-    }
-  };
+  const handleSignUp = () => {
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then(userCredential => {
+        const user = userCredential.user
+        console.log(user.email)
+      })
+      .catch(error => alert(error.message))
+  }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Sign Up</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Enter your name"
-        value={name}
-        onChangeText={(text) => setName(text)}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Enter your email"
-        value={email}
-        onChangeText={(text) => setEmail(text)}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Enter your password"
-        value={password}
-        onChangeText={(text) => setPassword(text)}
-        secureTextEntry
-      />
-      <Button title="Sign Up" onPress={handleSignUp} />
-      <Text style={styles.loginText}>
-        Already have an account?{' '}
-        <Text
-          style={styles.loginLink}
-          onPress={() => navigation.navigate('Login')}
+    <KeyboardAvoidingView 
+      style={styles.container}
+      behavior = 'padding'
+    >
+      <View style={styles.container}>
+        <Text style={styles.title}>Sign Up</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter your name"
+          value={name}
+          onChangeText={(text) => setName(text)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Enter your email"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Enter your password"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+          secureTextEntry
+        />
+      </View>
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          onPress={handleSignUp}
+          style={[styles.button, styles.buttonOutLine]}
         >
-          Log In
-        </Text>
-      </Text>
-    </View>
+          <Text style={styles.buttonOutLineText}>Register</Text>
+          // Return to login page if already have account
+          <Text style={styles.loginText}>Already have an account?{' '}
+          <Text 
+            style={styles.loginLink}
+            onPress={() => navigation.navigate('Login')}
+          >Log In</Text>
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
   );
 }
+
+export default SignUpScreen;
 
 const styles = StyleSheet.create({
   container: {
@@ -111,4 +94,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default SignUpScreen;
+
