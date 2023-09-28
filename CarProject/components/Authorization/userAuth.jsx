814784052f8ec1c791c8b15676b9409659483abe
hlnguyenlinh11 <112446306+hlnguyenlinh11@users.noreply.github.com>
2023-09-28 11:49:18 -0500

    userAuth

--- CarProject/components/Authorization/userAuth.jsx
+++ CarProject/components/Authorization/userAuth.jsx
@@ -1,2 +1,10 @@
 import React, {useEffect} from "react";
 import { getAuth, onAuthStateChange, User} from "firebase/auth"
+
+const auth = getAuth(); 
+
+export function useAuth() {
+    const [user, setUser] = React.useState<User>(); 
+
+    
+}
