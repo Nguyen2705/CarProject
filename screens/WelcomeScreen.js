5b4914e9f4eff24058fd1648fb2f000f51802ac2
hlnguyenlinh11 <112446306+hlnguyenlinh11@users.noreply.github.com>
2023-10-03 12:09:03 -0500

    SignUpScreen color

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -45,7 +45,7 @@ const styles = StyleSheet.create({
         flex: 1, 
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: 'yellow',
+        backgroundColor: '#faca63',
     },
     logocontainer: {
         alignItems: 'center', 
