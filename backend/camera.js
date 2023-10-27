08357dc34766ba90232792b1ad5b5cf11532bbb6
nguyen2705 <minhvu0312@gmail.com>
2023-10-26 20:24:44 -0500

    Fix Camera UI and name

--- backend/camera.js
+++ backend/camera.js
@@ -1,63 +1,118 @@
-import { Camera, CameraType } from 'expo-camera';
+import React from 'react';
+import { Camera } from 'expo-camera';
 import { useEffect, useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 
+export default function CameraScreen() {
+  const navigation = useNavigation();
 
-
-
-export default function camera() {
-  const [type, setType] = useState(CameraType.back);
-  const [permission, requestPermission] = Camera.useCameraPermissions();
+  const [type, setType] = useState(Camera.Constants.Type.front);
+  const [hasPermission, setHasPermission] = useState(null);
+  const [cameraRef, setCameraRef] = useState(null);
 
   useEffect(() => {
-    (async ()=> {
-      const { status } = await Camera.requestPermissionsAsync();
-      requestPermission(status === 'granted');
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === 'granted');
     })();
-  })
+  }, []);
 
-  if (permission === null) {
+  if (hasPermission === null) {
     return <View />;
   }
 
-  if (permission === false) {
-    return <Text> No access to camera</Text>;
-  }
-
-  if (!permission.granted) {
-    // Camera permissions are not granted yet
-    return (
-      <View style={styles.container}>
-        <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
-        <Button onPress={requestPermission} title="grant permission" />
-      </View>
-    );
+  if (hasPermission === false) {
+    return <Text>No access to the camera</Text>;
   }
 
   function toggleCameraType() {
-    setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
+    setType((currentType) =>
+      currentType === Camera.Constants.Type.back
+        ? Camera.Constants.Type.front
+        : Camera.Constants.Type.back
+    );
   }
 
+  const takePicture = async () => {
+    if (cameraRef) {
+      let photo = await cameraRef.takePictureAsync();
+      console.log(photo);
+    }
+  };
+
   return (
-    <View style={{ flex: 1 }}>
-      <View style={styles.cameraContainer}>
-        <Camera
-          style={styles.fixedRatio}
-          type={type}
-          ratio={'16:9'} />
-      </View>
-      <Button title="Flip Camera" style={styles.button} onPress={toggleCameraType}/>
-    </View>    
+    <View style={styles.container}>
+      <Camera
+        style={{ flex: 1 }}
+        type={type}
+        ref={(ref) => setCameraRef(ref)}
+        ratio="16:9"
+      >
+        <View style={styles.header}>
+          {/* Add a transparent header here */}
+        </View>
+        <View style={styles.cameraContainer}>
+          <TouchableOpacity style={styles.captureButton} onPress={takePicture}>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.iconsContainer}>
+          <TouchableOpacity
+            style={styles.icon}
+            onPress={() => {
+              navigation.goBack();
+            }}
+          >
+            <Ionicons name="close" size={32} color="white" />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.icon}
+            onPress={toggleCameraType}
+          >
+            <Ionicons name="camera-reverse" size={32} color="white" />
+          </TouchableOpacity>
+        </View>
+      </Camera>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: 'rgba(0, 0, 0, 0.3)', // Transparent background
+    height: 100,
+  },
   cameraContainer: {
     flex: 1,
-    flexDirection: 'row'
+    alignItems: 'center',
+    justifyContent: 'flex-end',
   },
-  fixedRatio: {
-    flex: 1,
-    aspectRatio: 16 / 9
-  }
+  captureButton: {
+    backgroundColor: 'yellow',
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+    borderWidth: 5,
+    borderColor: 'white',
+    marginBottom: 30,
+  },
+  iconsContainer: {
+    position: 'absolute',
+    top: 50,
+    left: 0,
+    right: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  icon: {
+    padding: 10,
+  },
 });
