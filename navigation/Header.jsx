7ac0ea4d71db5283ff5ddb86d23e9afa2903a212
nguyen2705 <minhvu0312@gmail.com>
2023-10-28 15:53:19 -0500

    Add settings button and Fix header UI

--- navigation/Header.jsx
+++ navigation/Header.jsx
@@ -79,7 +79,7 @@ const styles = StyleSheet.create({
     },
     headerBackground: {
         marginTop: 10, 
-        
+        backgroundColor: 'white',
         paddingVertical: 0, 
         flexDirection: 'row', 
         justifyContent: 'space-between',
