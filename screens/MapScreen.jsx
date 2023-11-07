56c442369e41495f117df14e69c14ba3d4836358
hlnguyenlinh11 <112446306+hlnguyenlinh11@users.noreply.github.com>
2023-11-07 12:08:48 -0600

    MapScreen.jsx

--- screens/MapScreen.jsx
+++ screens/MapScreen.jsx
@@ -1,20 +1,36 @@
-import React from 'react'; 
-import { View, Text, StyleSheet } from 'react-native'; 
+import React from 'react';
+import MapView, { Marker } from 'react-native-maps';
+import { StyleSheet, View } from 'react-native';
 
 const MapScreen = () => {
-    return (
-        <View style={styles.container}>
-        <Text>Map Screen Content Goes Here</Text>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 32.705002,
+          longitude: -97.122780,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        <Marker
+          coordinate={{ latitude: 32.705002, longitude: -97.122780 }}
+          title="Marker Title"
+          description="Marker Description"
+        />
+      </MapView>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
+  container: {
+    flex: 1,
+  },
+  map: {
+    flex: 1,
+  },
 });
 
-export default MapScreen; 
+export default MapScreen;
